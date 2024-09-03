3902412dc0a61a853b60adda643f431ba17a94f4
Aira Jena <airajena.hack@gmail.com>
2024-09-03 18:06:53 +0530

    feat: Add rate limiter

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 50, 
+    message: "Too many requests from this IP, please try again after a minute.",
+    headers: true, 
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(limiter);
 
 app.use('/api', apiRoutes);
 
