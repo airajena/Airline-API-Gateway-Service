c7619df2933bcf2e8b0d2f40e767b0fc88593f88
Aira Jena <airajena.hack@gmail.com>
2024-09-04 17:10:01 +0530

    feat: Add proxy middleware for booking service

--- src/index.js
+++ src/index.js
@@ -17,8 +17,17 @@ const limiter = rateLimit({
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(limiter);
-app.use('/flightsService', createProxyMiddleware({ target: ServerConfig.FLIGHT_SERVICE, changeOrigin: true, pathRewrite: {'^/flightsService' : '/'} }));
-app.use('/bookingService', createProxyMiddleware({ target: ServerConfig.BOOKING_SERVICE, changeOrigin: true }));
+app.use('/flightsService', createProxyMiddleware({ 
+    target: ServerConfig.FLIGHT_SERVICE, 
+    changeOrigin: true, 
+    pathRewrite: {'^/flightsService' : '/'} 
+}));
+
+app.use('/bookingService', createProxyMiddleware({
+    target: ServerConfig.BOOKING_SERVICE,
+    changeOrigin: true,
+    pathRewrite: {'^/bookingService' : '/'}
+}));
 
 app.use('/api', apiRoutes);
 
